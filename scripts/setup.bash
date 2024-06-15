417bc1a7fd6c24f814c2dc4d45e750439fe49e55
incrementalcapital <incrementalcapital@icloud.com>
2024-06-15 04:41:00 +0000

    add the commands that allow the user to input required authentication credentials

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,9 +1,14 @@
 #! /bin/bash
+git config --global core.editor "vim"
+git config --global --edit
+git commit --amend --reset-author
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install nodejs npm && echo "installed node version $(node -v)" && echo "installed npm version $(npm -v)"
 npm install typescript
 npm install --save-dev @types/node
 sudo npm install -g http-server
+mkdir authentication
+vi authentication/openai
 npx tsc
 http-server
